5423edf7ede12f4897d14885de9729295f6abdc1
HectorSanchezUK <hector@kaerustudios.com>
2014-07-12 13:08:49 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,94 @@
+3DOM
+====
+
+## Instructions:
+
+The very top blue bar, will be an update bar where we can post messages for updates to the app
+
+The menu is going to be fixed so that is visible when scrolling at all times
+
+There are a lot of expanding sections depending on the amount of content within that section, so this will need to be considered and have a nice solution that won't break the page
+
+There are some of the graphical content that will be needed like:
+
++ Message number next to icon
++ Darkened menu uption when selected
++ "+" icons to "-" icons when a section is expanded
+
+### Hover CSS:
+
++ On red buttons with white text, change to blue buttons with white text
++ On white with grey text, change red with white icons to blue with white icons
++ App data list content, highlight the row on hover
++ App data list + and - icons change to red
++ App data list titles - change to red. Blue when selected
++ Any clickable images to have the highlight hover effect
++ Info sections like project groups, history, presentations have a light grey background behind each row on hover
++ Let us know button at footer, change to light blue
++ Any grey icons on a dark background, change to white
++ White icons on grey, change to blue
++ Red icons in white, change to blue
+
+App tools panel icons and hover to be decided when the icons are completed in the next few days
+
+Images - open up in lightbox
+
+### App page
+Tools tabs - small icons on the left to control the tab that is viewable
+
+Greyed out if not active
+
+Icon to be bright and coloured if active
+
+## Feedback
+
+####General
+* ~~Add Viewport Meta tag~~
+* ~~Make footer full-width~~
+* ~~Title on pages is not changin as it should, "6 Projects" in the header should change to the corresponding page title.~~
+* ~~ Can we change the mobile menu to an off-canvas menus. You can use http://mmenu.frebsite.nl/ if you want, or whatever you feel more comfortable with but give it that interaction.~~ (Postponed)
+
+####Get A Quote
+* ~~Check responsive styles~~
+
+####Hire A Designer / Logos
+* ~~Can you just add the interaction for the file upload zone~~
+
+####Dynamise the pages
+* ~~We need the blue bar at the top of every page to appear on load and add a 'close' or 'x' link to make it disappear~~
+
+####Dashboard / Presentations
+* ~~The responsive style for 'You might like' and 'Projects Gallery' areas in the Dashboard are not consistent with the ones for 'You might like' area in the Presentations page~~
+
+####Support / Support Selected
+* ~~Can you show with dummy content the interaction between the 6 categories.~~
+
+
+## Tasks
+
+### To do
+
+
+### Doing
+* Project Settings (*Project_Settings.psd*) (Damien)
+* Presentations Driver (*Presentations_Driver.psd*) (Eric) --> Faire le slider
+
+### To check
+* Presentations (*Pierre-Jean*)
+* Get A Quote (*GetAQuote.psd*) (Quentin)
+* Hire A Designer (Pierre-Jean)
+* Logos (*Logos.psd*) (Eric)
+* Dynamise the pages (Pierre-Jean)
+* Dashboard (*Dashboard.psd*) (Damien)
+* Support Selected (*Support_Selected.psd*) (*Eric*)
+* Project Settings Activity (*Project_Settings_Activity.psd*) (Quentin)
+* Project Settings Comments (*Project_Settings_Comments.psd*) (Quentin)
+* Convert Design (*ConvertDesign.psd*) (Boris)
+* Project Selected (*Project_Selected.psd*) (Boris)
+
+### Done
+
+* Support Landing (*Eric*)
+* Sign In (*SignIN.psd*) (Eric)
+
+
---
+++ js/main.js
@@ -0,0 +1,240 @@
+// Create the app
+var ThreeDOMApp = angular.module('ThreeDOMApp', ['ngRoute', 'ngAnimate']);
+
+// Configure the routes
+ThreeDOMApp.config(function($routeProvider) {
+
+    $routeProvider  // Projects route
+                    .when('/projects', {
+                                    title: 'Projects',
+                                    templateUrl : 'pages/projects.html',
+                                    controller  : 'projectsController'})
+                    // Logos route
+                    .when('/logos', {
+                                    title: 'Logos',
+                                    templateUrl : 'pages/logos.html',
+                                    controller  : 'logosController'})
+                    // Presentations route
+                    .when('/presentations', {
+                                    title: 'Presentations',
+                                    templateUrl : 'pages/presentations.html',
+                                    controller  : 'presentationsController'})
+                    // Quotes route
+                    .when('/quotes', {
+                                    title: 'Quotes',
+                                    templateUrl : 'pages/quotes.html',
+                                    controller  : 'quotesController'})
+                    // Support route
+                    .when('/support', {
+                                    title: 'Support',
+                                    templateUrl : 'pages/support.html',
+                                    controller  : 'supportController'})
+
+                    // Convert route
+                    .when('/convert', {
+                                    title: 'Convert',
+                                    templateUrl : 'pages/convert.html',
+                                    controller  : 'convertController'})
+
+                    // Active project route
+                    .when('/projectselected', {
+                                    title: 'Project selected',
+                                    templateUrl : 'pages/projectSelected.html',
+                                    controller  : 'projectSelectedController'})
+									
+                    // Setting project route
+                    .when('/projectsettings', {
+                                    title: 'Project settings',
+                                    templateUrl : 'pages/projectsettings.html',
+                                    controller  : 'projectSettingController'})
+
+
+                    // Hire a designer route
+                    .when('/hieradesigner', {
+                                    title: 'Hire a designer',
+                                    templateUrl : 'pages/hieradesigner.html',
+                                    controller  : 'hieradesignerController'})
+                    // Presentations driver route
+                    .when('/presentationsdriver', {
+                                    title: 'Presentations Driver',
+                                    templateUrl : 'pages/presentationsdriver.html',
+                                    controller  : 'presentationsdriverController'})
+                    // Default route
+                    .otherwise({
+                                    title: 'Dashboard',
+                                    templateUrl : 'pages/dashboard.html',
+                                    controller  : 'dashboardController'})
+
+});
+
+
+// Configure the controllers
+            // Main controller
+ThreeDOMApp .controller('mainController', function($scope, $route, $rootScope, $location) {
+                //Change page title, based on Route information
+                $rootScope.$on('$routeChangeSuccess',
+                    function(event, toState, toParams, fromState, fromParams){
+                        $scope.title = $route.current.title;
+                        $scope.loading = false;
+                    });
+                $rootScope.$on('$routeChangeStart', function(scope, next, current){
+                   $scope.loading = true;
+                });
+                $scope.isActive = function(route) {
+                    return route === $location.path();
+                };
+                $scope.sign_in = false;
+                $scope.alert = true;
+            })
+            // Dashboard controller
+            .controller('dashboardController', function($scope) {
+            })
+            // Projects controller
+            .controller('projectsController', function($scope) {
+            })
+            .controller('convertController', function($scope) {
+                var dropEl = angular.element('.dd_zone')[0];
+                dropEl.addEventListener("dragenter", function(e) {
+                    e = e || event;
+                    e.preventDefault();
+                console.log("DragEnter event");
+                }, false);
+            })
+            // Logos controller
+            .controller('logosController', function($scope) {
+                var dropEl = angular.element('.dd_zone')[0];
+                dropEl.addEventListener("dragenter", function(e) {
+                    e = e || event;
+                    e.preventDefault();
+                console.log("DragEnter event");
+                }, false);
+            })
+            // Presentations controller
+            .controller('presentationsController', function($scope) {
+            })
+            // Quotes controller
+            .controller('quotesController', function($scope) {
+            })
+			// Project Setting Controller
+            .controller('projectSettingController', function($scope) {
+                $scope.tab=1;
+			})
+            // Support controller
+            .controller('supportController', function($scope) {
+                $scope.more_info = false;
+                $scope.getting_started = false;
+                $scope.getting_started_item2 = false;
+
+                $scope.isHidden = function() {
+                    return ($scope.more_info && $scope.getting_started);
+                }
+            })
+            // project selected controller
+            .controller('projectSelectedController', function($scope) {
+            })
+            // Hire a designer controller
+            .controller('hieradesignerController', function($scope) {
+                var dropEl = angular.element('.dd_zone')[0];
+                dropEl.addEventListener("dragenter", function(e) {
+                    e = e || event;
+                    e.preventDefault();
+                console.log("DragEnter event");
+                }, false);
+            })
+            // Presentations driver controller
+            .controller('presentationsdriverController', function($scope, $document) {
+                
+                var dropEl = angular.element('.dd_zone')[0];
+                dropEl.addEventListener("dragenter", function(e) {
+                    e = e || event;
+                    e.preventDefault();
+                console.log("DragEnter event");
+                }, false);
+
+                var mediaPlayer;
+                var progressBar = angular.element('#progress-video')[0];
+
+                initialiseMediaPlayer();
+                $scope.initialiseMediaPlayer = initialiseMediaPlayer;
+                function initialiseMediaPlayer() {
+                    mediaPlayer = angular.element('#media-video')[0];
+                    mediaPlayer.controls = false;
+                    mediaPlayer.addEventListener('timeupdate', updateProgressBar, false);
+                }
+                $scope.updateProgressBar = updateProgressBar;
+                function updateProgressBar() {
+                    var percentage = Math.floor((100 / mediaPlayer.duration) *
+                    mediaPlayer.currentTime);
+                    progressBar.style.width = percentage+'%';
+                    angular.element('#total-duration')[0].innerHTML = ("0" + Math.round((mediaPlayer.duration)/60)).slice(-2)+':'+("0" + Math.round((mediaPlayer.duration)-(mediaPlayer.duration)/60)).slice(-2);
+                    angular.element('#current-time')[0].innerHTML = ("0" + Math.round((mediaPlayer.currentTime)/60)).slice(-2)+':'+("0" + Math.round((mediaPlayer.currentTime)-(mediaPlayer.currentTime)/60)).slice(-2);;
+                }
+                $scope.togglePlayPause = togglePlayPause;
+                function togglePlayPause() {
+                    var btn = angular.element('#play-pause-button')[0];
+                    if (mediaPlayer.paused || mediaPlayer.ended) {
+                        btn.title = 'pause';
+                        btn.innerHTML = 'pause';
+                        btn.className = 'pause';
+                        mediaPlayer.play();
+                    }
+                    else {
+                        btn.title = 'play';
+                        btn.innerHTML = 'play';
+                        btn.className = 'play';
+                        mediaPlayer.pause();
+                    }
+                }
+                $scope.changeButtonType = changeButtonType;
+                function changeButtonType(btn, value) {
+                    btn.title = value;
+                    btn.innerHTML = value;
+                    btn.className = value;
+                }
+                $scope.stopPlayer = stopPlayer;
+                function stopPlayer() {
+                    mediaPlayer.pause();
+                    mediaPlayer.currentTime = 0;
+                }
+                $scope.changeVolume = changeVolume;
+                function changeVolume(direction) {
+                    if (direction === '+')
+                        mediaPlayer.volume += mediaPlayer.volume == 1 ? 0 : 0.1;
+                    else 
+                        mediaPlayer.volume -= (mediaPlayer.volume == 0 ? 0 : 0.1);
+                    mediaPlayer.volume = parseFloat(mediaPlayer.volume).toFixed(1);
+                }
+                $scope.toggleMute = toggleMute;
+                function toggleMute() {
+                    var btn = angular.element('#mute-button')[0];
+                    if (mediaPlayer.muted) {
+                        changeButtonType(btn, 'mute');
+                        mediaPlayer.muted = false;
+                    }
+                    else {
+                        changeButtonType(btn, 'unmute');
+                        mediaPlayer.muted = true;
+                    }
+                }
+                $scope.replayMedia = replayMedia;
+                function replayMedia() {
+                    resetPlayer();
+                    mediaPlayer.play();
+                }
+                $scope.resetPlayer = resetPlayer;
+                function resetPlayer() {
+                    progressBar.value = 0;
+                    mediaPlayer.currentTime = 0;
+                    changeButtonType(playPauseBtn, '▶');
+                }
+
+                mediaPlayer.addEventListener('play', function() {
+                    var btn = angular.element('#play-pause-button')[0];
+                    changeButtonType(btn, 'll');
+                }, false);
+
+                mediaPlayer.addEventListener('pause', function() {
+                    var btn = angular.element('#play-pause-button')[0];
+                    changeButtonType(btn, '▶');
+                }, false);
+            });
